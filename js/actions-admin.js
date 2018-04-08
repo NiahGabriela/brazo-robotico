ed4a0ea959ddbd15d2f5b6c7b34d2bc3d5cb754f
NiahGabriela <dghg.12@hotmail.com>
2018-04-08 17:10:12 -0500

    antes de agregar el jquery

---
+++ js/actions-admin.js
@@ -0,0 +1,15 @@
+let passAdmin = document.getElementById('password');
+
+passAdmin.addEventListener('blur',checkPass);
+
+function checkPass()
+{
+  if (this.value==='donshirito')
+  {
+      console.log('don chilito anda de buenas');
+  }
+  else
+  {
+
+  }
+}
