7d29a3e8bc658c70cdc896868789d66728fe13f9
NiahGabriela <dghg.12@hotmail.com>
2018-04-09 22:16:28 -0500

    finished the login for the admin, with js

--- js/actions-admin.js
+++ js/actions-admin.js
@@ -1,73 +1,27 @@
 
 let passAdmin = document.getElementById('password');
+var notTilCorrect = document.getElementById('signin');
 
 passAdmin.addEventListener('blur',checkPass);
+notTilCorrect.addEventListener('click',controlButton);
+
+function controlButton (event)
+{
+  event.preventDefault();
+}
 
 function checkPass(evento)
 {
   console.log(this.value);
-  if (this.value==='donshirito')
+  if (this.value ==='donshirito')
   {
-      console.log('don chilito anda de buenas');
+    notTilCorrect.removeEventListener('click', controlButton, false);
+    console.log('don chilito anda de buenas');
+    document.getElementById("password").className = document.getElementById("password").className.replace(" error", "");
   }
   else
   {
-      evento.preventDefault();
-      generateError();
+    document.getElementById("password").className = document.getElementById("password").className + " error";
+    document.getElementById("errorPass").style.visibility = "visible";
   }
 }
-
-// console.log($('input'));
-let generateError = function generateError ()
-{
-  console.log($('input'));
-  alert('Contraseña incorrecta');
-  element.addClass('error');
-  element.insertAfter();
-  $('<span> Vuelva a ingresar la contraseña</span>').insertAfter(element);
-}
-//
-// let validateForm = function validateForm(e)
-// {
-//   let button =$(this);
-//   let pristine = true;
-//   //obtain form
-//   let form = button.parent('form');
-//   // obtener todos los elementos required
-//   let required = form.find('*[required]');
-//
-//   pristine = validateRequiredFields(required);
-//
-//   if(pristine)
-//   {
-//     e.preventDefault();
-//   }
-// }
-//
-// let validateRequiredFields = function validateRequiredFields (fields)
-// {
-//   let pristine = true;
-//   $.each(fields, funtion()
-//   {
-//     let element = $(this);
-//     console.log(this);
-//
-//     if(element.is('input') && element.val() !== 'donshirito')
-//     {
-//       generateError(element, 'required');
-//       pristine = false;
-//     }
-//
-//     // para cuando pongas un elemento de tipo select va esto,
-//     // y procurar que cuando no se haya selecionado nada ponerle un value de 0
-//   });
-//       return pristine;
-// }
-//
-// let generateError = function generateError(element,typeError)
-// {
-//   alert('This' + element + 'field is required');
-//   element.addClass('error');
-//   $('<span> This field is required </span>').insertAfter(element);
-//
-// }
