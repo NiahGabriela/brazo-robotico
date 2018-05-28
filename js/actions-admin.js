af900a2b2f3d741006245b46644d2357737cedc6
NiahGabriela <dghg.12@hotmail.com>
2018-05-27 20:27:33 -0500

    con json de regreso

--- js/actions-admin.js
+++ js/actions-admin.js
@@ -37,30 +37,29 @@ function checkPass(evento)
 
 $('document').ready(function() {
 
-
-  // $.get('http://localhost:3000/users', function( data ) {
-  //
-  //   console.log(data);
+  $.get('http://localhost:3000/users', function( data ) {
+    console.log('bpollo');
+    console.log(data);
     var html = ``;
-html += `<label><input type="checkbox" class="block-bad"/> wowoow </label>`;
-html += `<label><input type="checkbox" class="block-bad"/> stay girls </label>`;
-    // $.each(data, function(index, value) {
-    //   html += `<label><input type="checkbox" class="block-bad"/> ${value.name} </label>`;
-    //     // html += `<td>`;
-    //   //   html += `<input class="users-list" type="checkbox" name="selected" value="${value.id}">`;
-    //   //   html += `</td>`;
-    //   //   html += `<td>${value.name}</td>`;
-    //   //   html += `<td>${value.last_name}</td>`;
-    //   //   html += `<td>${value.email}</td>`;
-    //   //   html += `<td>${value.phone}</td>`;
-    //   //   html += `<td>${value.score}</td>`;
-    //   // html += `</tr>`;
-    // });
+    html += `<label><input type="checkbox" class="block-bad"/> wowoow </label>`;
+    html += `<label><input type="checkbox" class="block-bad"/> stay girls </label>`;
+    $.each(data, function(index, value) {
+      html += `<label><input type="checkbox" class="block-bad"/> ${value.name} </label>`;
+        // html += `<td>`;
+      //   html += `<input class="users-list" type="checkbox" name="selected" value="${value.id}">`;
+      //   html += `</td>`;
+      //   html += `<td>${value.name}</td>`;
+      //   html += `<td>${value.last_name}</td>`;
+      //   html += `<td>${value.email}</td>`;
+      //   html += `<td>${value.phone}</td>`;
+      //   html += `<td>${value.score}</td>`;
+      // html += `</tr>`;
+    });
     console.log(html);
     console.log('señales de vida');
     $('div .users-list').append(html);
 
-  // });
+  });
   ////////////////********** adminCheckboxSelected ******/////////////////
 
   $( '.block-bad' ).on( 'click', function()
@@ -75,10 +74,10 @@ html += `<label><input type="checkbox" class="block-bad"/> stay girls </label>`;
         //$(this).prop('disabled', true).parent().addClass('block-user-bad');
         confirm("¿Desea bloquear al usuario? ");
 
-        $.put( 'http://localhost:3000/users/ ', function()
-        {
-
-        });
+        // $.put( 'http://localhost:3000/users/ ', function()
+        // {
+        //
+        // });
 
       }
       else
