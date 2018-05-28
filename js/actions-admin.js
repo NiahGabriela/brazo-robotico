3273557769e6ba6ba96088cffe3fc8b9b0474c7c
NiahGabriela <dghg.12@hotmail.com>
2018-05-27 19:04:22 -0500

    aun falla el put, pero el post si funciona

--- js/actions-admin.js
+++ js/actions-admin.js
@@ -33,22 +33,59 @@ function checkPass(evento)
   }
 }
 
-// adminCheckboxSelected
+///carga los usuarios de la base de datos
 
-$( '.block-bad' ).on( 'click', function()
-{
-    if( $(this).is(':checked') )
-    {
-      //$(this).addClass('block-user-bad');
-      $(this).parent().addClass('block-user-bad');
+$('document').ready(function() {
 
-      //$(this).prop('disabled', true).parent().addClass('block-user-bad');
-      confirm("¿Desea bloquear al usuario? ");
-    }
-    else
-    {
-      $(this).parent().removeClass('block-user-bad');
-            //$(this).prop('disabled', false).parent().addClass('block-user-bad');
-        confirm("¿Desea desbloquear al usuario? ");
-    }
+
+  // $.get('http://localhost:3000/users', function( data ) {
+  //
+  //   console.log(data);
+    var html = ``;
+html += `<label><input type="checkbox" class="block-bad"/> wowoow </label>`;
+html += `<label><input type="checkbox" class="block-bad"/> stay girls </label>`;
+    // $.each(data, function(index, value) {
+    //   html += `<label><input type="checkbox" class="block-bad"/> ${value.name} </label>`;
+    //     // html += `<td>`;
+    //   //   html += `<input class="users-list" type="checkbox" name="selected" value="${value.id}">`;
+    //   //   html += `</td>`;
+    //   //   html += `<td>${value.name}</td>`;
+    //   //   html += `<td>${value.last_name}</td>`;
+    //   //   html += `<td>${value.email}</td>`;
+    //   //   html += `<td>${value.phone}</td>`;
+    //   //   html += `<td>${value.score}</td>`;
+    //   // html += `</tr>`;
+    // });
+    console.log(html);
+    console.log('señales de vida');
+    $('div .users-list').append(html);
+
+  // });
+  ////////////////********** adminCheckboxSelected ******/////////////////
+
+  $( '.block-bad' ).on( 'click', function()
+  {
+      if( $(this).is(':checked') )
+      {
+        var $e = $(this);
+        var selectedLabel = $e.parent().text();
+        console.log(selectedLabel);
+        $(this).parent().addClass('block-user-bad');
+
+        //$(this).prop('disabled', true).parent().addClass('block-user-bad');
+        confirm("¿Desea bloquear al usuario? ");
+
+        $.put(`http://localhost:3000/users/ ${selectedLabel}`, function()
+        {
+
+        });
+
+      }
+      else
+      {
+        $(this).parent().removeClass('block-user-bad');
+              //$(this).prop('disabled', false).parent().addClass('block-user-bad');
+          confirm("¿Desea desbloquear al usuario? ");
+      }
+  });
 });
